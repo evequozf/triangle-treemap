600b13e3aee3c656891b67e42583ad5032facb16
evequozf <florian.evequoz@gmail.com>
2015-08-03 22:59:19 +0200

    Bug fix
    Bug in squarified layout (last sibling sometimes not drawn) + tests

--- triangle-treemap.js
+++ triangle-treemap.js
@@ -8,6 +8,14 @@ var sqdist = function(a, b) {
 }
 
 /*
+// Three points are a counter-clockwise turn if ccw > 0, clockwise if
+// ccw < 0, and collinear if ccw = 0 because ccw is a determinant that
+// gives twice the signed  area of the triangle formed by p1, p2 and p3.
+// Source: https://en.wikipedia.org/wiki/Graham_scan
+var ccw = function (p1, p2, p3) {
+  return (p2.x - p1.x)*(p3.y - p1.y) - (p2.y - p1.y)*(p3.x - p1.x);
+}
+
 // returns distance between two points
 var dist = function(a, b) {
   return Math.sqrt( sqdist(a, b) );
@@ -76,22 +84,16 @@ var f = function(p0, p1, p2, targetArea) {
 // TREEMAP FUNCTION (as layout function -> adds a 'triangle' field in the data)
 
 /// treemap in triangles using a slice and dice fashion
-var treemap = function(data, trng, lvl, squarify) {
+var treemap = function(data, trng, squarify) {
   
   if( typeof(squarify) === 'undefined' ) squarify = false;
 
   // console.log(data.name + " " + data.key);
   // console.log(lvl);
-  
-  // insert new property to current data : current triangle
-  data.triangle = trng;
 
   // build treemap layout for children
-  if(data.children == null) {
-    // console.log("children null -> exit")
-    return;
-  } else {
-    
+  if(data.children) {
+
     // sort children descending
     var children = data.children.sort(function(a,b){ return d3.descending(a.value, b.value); });
     var t = trng;          // current triangle to split
@@ -100,30 +102,31 @@ var treemap = function(data, trng, lvl, squarify) {
     // process all siblings
     for(var i = 0; i < children.length; i++) {
       
-      // console.log("------ Treemap for-loop -----");
-      // console.log("parent value = "+data.value);
-      // console.log("this value   = "+children[i].value);
-      // console.log("p    = "+(p));
-      // console.log("target prop = "+(children[i].value / p));
+     // console.log("------ Treemap for-loop -----");
+     // console.log("parent value = "+data.value);
+     // console.log("this value   = "+children[i].value);
+     // console.log("p    = "+(p));
+     // console.log("target prop = "+(children[i].value / p));
       
       var s = surface(t),
           c = children[i],
           targetS = s * (c.value / p);
       
       // 'Squarify'-like variant : split remaining triangle along biggest side
-      if(squarify) {  
-        // what's the biggest side ? saure distance comparison
+      // don't apply squarify on last child -> otherwise may not split correctly
+      if ( (squarify) && (i != children.length - 1) ) {  
+        // what's the biggest side ? sqaure distance comparison
         var t0d = sqdist(t[1], t[2]),
             t1d = sqdist(t[0], t[2]),
             t2d = sqdist(t[0], t[1]),
             mx = Math.max(t0d, t1d, t2d);
         if(mx === t0d) {
-            // -> split side opposite to t0 (already the case with current t)
+          //t = [ t[0], t[1], t[2] ]; // -> split side opposite to t0 (already the case with current t)
         } else if(mx === t1d) {
           t = [ t[1], t[2], t[0] ]; // split side opposite to t1
         } else if(mx === t2d) {
           t = [ t[2], t[0], t[1] ]; // split side opposite to t2
-        }
+        }  
       }
 
       // find split point, build t1 and t2 as subtriangle around splitPoint
@@ -135,11 +138,18 @@ var treemap = function(data, trng, lvl, squarify) {
       //   +" actual proportion : "+(surface(t1)/s));
 
       // recursive call under current sibling within t1 
-      treemap(c, [t1[1], t1[2], t1[0]], lvl+1, squarify); //slice & dice (useless if squarify)
+      // TODO : possibly buggy for last child ?
+      treemap(c, [t1[1], t1[2], t1[0]], squarify); //slice & dice (useless if squarify)
 
       // process siblings
       t = t2;          // siblings will split remaining triangle ...
       p = p - c.value; // ... with adapted proportion: parent value minus already splitted siblings
     }
   }
+
+  // insert new property to current data : current triangle 
+  // FIXME : outside ? should it be
+  // just the return value of this function instead ?
+  data.triangle = trng;
+
 }
